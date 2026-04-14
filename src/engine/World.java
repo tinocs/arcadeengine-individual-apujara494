12364c353a0ef2eb67fe5aef7c29c6b0bb7ff6cf
Aditi Pujara <apujara494@student.fuhsd.org>
2026-04-14 15:30:37 -0700

    i worked on the world class, still not fully working tho

--- src/engine/World.java
+++ src/engine/World.java
@@ -1,10 +1,18 @@
 package engine;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import javafx.animation.AnimationTimer;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.event.EventHandler;
 import javafx.scene.layout.Pane;
+import javafx.scene.Node;
+import javafx.scene.Scene;
 import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 public abstract class World extends Pane{
 //	An AnimationTimer that calls act() on the world first and then on each actor in the world every frame.
 //	A variable that keeps track of whether the timer is running.
@@ -12,20 +20,103 @@ public abstract class World extends Pane{
 //	Variable(s) that keep track of whether the width and height have been set. Since width and height of Nodes is always 0 until layout is done (some time after show() is called on the stage), you will need a way to determine when the World has been given a real width and height so you can use the getWidth() and getHeight() methods when positioning your actors.
 	private AnimationTimer timer;
 	private boolean isTimerStopped;
-	
 	private boolean isWidthSet;
 	private boolean isHeightSet;
-	private  ArrayList<KeyCode> keysPressed = new ArrayList<>();
+	private  Set <KeyCode> keysPressed;
 
 	public World() {
 		isWidthSet = false;
 		isHeightSet = false;
 		
-		isTimerStopped = true;
+		keysPressed = new HashSet<KeyCode>();
 		
-		//timer = new AnimationTimer();
+		isTimerStopped = false;
 		
-	}
+		widthProperty().addListener(new ChangeListener<Number>() {
+
+			@Override
+			public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
+				if(newValue.intValue()>0) {
+					isWidthSet = true;
+				}
+				if(isWidthSet && isHeightSet) {
+					onDimensionsInitialized();
+				}
+			}
+		   
+		    
+		});
+		
+		heightProperty().addListener(new ChangeListener<Number>() {
+
+			@Override
+			public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
+				if(newValue.intValue()>0) {
+					isHeightSet = true;
+				}
+				if(isWidthSet && isHeightSet) {
+					onDimensionsInitialized();
+				}
+			}
+		   
+		    
+		});
+		
+		sceneProperty().addListener(new ChangeListener<Scene>() {
+
+			@Override
+			public void changed(ObservableValue<? extends Scene> observable, Scene oldValue, Scene newValue) {
+				// TODO Auto-generated method stub
+				requestFocus();
+				
+				
+			}
+		  
+		   
+		});
+		 
+		setOnKeyPressed(new EventHandler<KeyEvent>() {
+
+			@Override
+			public void handle(KeyEvent event) {
+				// TODO Auto-generated method stub
+				keysPressed.add(event.getCode());
+			}
+		   
+		});
+		
+		setOnKeyReleased(new EventHandler<KeyEvent>() {
+
+			@Override
+			public void handle(KeyEvent event) {
+				// TODO Auto-generated method stub
+				keysPressed.remove(event.getCode());
+			}
+		   
+		});
+		
+		timer = new AnimationTimer() {
+
+			@Override
+			public void handle(long now) {
+				// TODO Auto-generated method stub
+				act(now);
+				
+				for(int i = 0;i<getChildren().size();i++) {
+					Actor x = (Actor) getChildren().get(i);
+					if(x.getClass().equals(Actor.class)) {
+						x.act(now);
+					}
+					
+				}
+				
+			}
+			
+		};
+		
+		}
+		
+	
 	public abstract void act(long now);
 	
 	public void	add(Actor actor) {
@@ -35,7 +126,14 @@ public abstract class World extends Pane{
 		return null;
 	}
 	public <A extends Actor> java.util.List<A> getObjectsAt(double x, double y, java.lang.Class<A> cls){
-		return null;
+		List<A> list = new ArrayList<A>();
+		for(Node n: getChildren()) {
+			if(cls.isInterface()) {
+				list.add((A)n);
+			}
+		}
+		
+		return list;
 	}
 	public boolean	isKeyPressed(javafx.scene.input.KeyCode code) {
 		return false;
@@ -50,12 +148,14 @@ public abstract class World extends Pane{
 
 	}
 	public void	start() {
-		
+		timer.start();
+		isTimerStopped = false;
 	}
 	public void	stop() {
-		
+		timer.stop();
+		isTimerStopped = true;
+
 	}
-	//private class widthListener
 	
-	// private class heightListener
 }
+
