d047f301ff2f0727ecfcde39bd2ccd65ef649868
Aditi Pujara <apujara494@student.fuhsd.org>
2026-05-02 19:28:24 -0700

    added pause

--- src/breakout/Ball.java
+++ src/breakout/Ball.java
@@ -1,12 +1,16 @@
 package breakout;
+
 import java.io.File;
+import java.util.List;
 
 import engine.Actor;
-import javafx.scene.image.Image;	
-public class Ball extends Actor{
+import javafx.scene.image.Image;
+
+public class Ball extends Actor {
 	int dx;
 	int dy;
-	public  Ball() {
+
+	public Ball() {
 		dx = 2;
 		dy = 3;
 		String path = getClass().getClassLoader().getResource("resources/ball.png").toString();
@@ -14,57 +18,83 @@ public class Ball extends Actor{
 		setImage(img);
 
 	}
+
 	@Override
 	public void act(long now) {
-		move(dx,dy);
-		// bounce of four walls
-		if(getX()<0) {
-			dx = -dx;
-		}
-		if(getY()<0) {
-			dy=-dy;
-		}
-		if(getX()>getWorld().getWidth()- getImage().getWidth()) {
-			dx = -dx;
-		}
-		if(getY()>getWorld().getHeight()- getImage().getHeight()) {
-			dy = -dy;
-			BallWorld bw = (BallWorld) getWorld();
-			Score s = bw.getScore();
-			int newScore = s.getScore() - 1000;
-			s.setScore(newScore);
-			
-		}
-		// After moving and bouncing off of walls during the act() step in the Ball class, add code so that a Ball will reverse its y-direction whenever it intersects a Paddle.
 
-		Paddle p = getOneIntersectingObject(Paddle.class);
-		if(p!=null) {
-			dy = -dy;
-		}
-		
-		Brick b = getOneIntersectingObject(Brick.class);
-		if(b!=null) {
-			dy = -dy;
-			BallWorld bw = (BallWorld) getWorld();
-			Score s = bw.getScore();
-			int newScore = s.getScore() + 100;
-			s.setScore(newScore);
-			
-			if(getX()>b.getX() && getX()<b.getX()+b.getWidth()) {
+//		In the act() method of every actor you want to pause, including the world, add the following logic:
+//			if (((BallWorld)getWorld()).isPaused()) // If the world is paused
+//				Do whatever you want when the game is paused.  Doing nothing is fine.
+//			      In my demo, I move the ball wherever the paddle is.
+//			else
+//				Move/act as usual
+
+		if (((BallWorld) getWorld()).isPaused()) {
+			  BallWorld ballworld = (BallWorld) getWorld();
+		        List<Paddle> p = ballworld.getObjects(Paddle.class);
+			  if(p.size()>0) {
+				  Paddle p1 = p.get(0);
+				  setX(p1.getX() + p1.getWidth()/2 - getImage().getWidth()/2);
+				  setY(p1.getY() - getImage().getHeight() - 2); 
+			  }
+
+		} else {
+
+			move(dx, dy);
+			// bounce of four walls
+			if (getX() < 0) {
+				dx = -dx;
+			}
+			if (getY() < 0) {
 				dy = -dy;
 			}
-			//else if Ball y-coord is between the Brick's top and bottom edges
-			//reverse x-direction
+			if (getX() > getWorld().getWidth() - getImage().getWidth()) {
+				dx = -dx;
+			}
+			if (getY() > getWorld().getHeight() - getImage().getHeight()) {
+				dy = -dy;
+				BallWorld bw = (BallWorld) getWorld();
+				Score s = bw.getScore();
+				int newScore = s.getScore() - 1000;
+				s.setScore(newScore);
+				  bw.setisPaused(true);
+				// bw.setlife--;
 
-			else if(getY()>b.getY() && getY()<b.getY()+b.getHeight()){
-				dx=-dx;
 			}
-			else {
-				dy=-dy;
-				dx=-dx;
+			// After moving and bouncing off of walls during the act() step in the Ball
+			// class, add code so that a Ball will reverse its y-direction whenever it
+			// intersects a Paddle.
+
+			Paddle p = getOneIntersectingObject(Paddle.class);
+			if (p != null) {
+				dy = -dy;
 			}
+
+			Brick b = getOneIntersectingObject(Brick.class);
+			if (b != null) {
+				dy = -dy;
+				BallWorld bw = (BallWorld) getWorld();
+				Score s = bw.getScore();
+				int newScore = s.getScore() + 100;
+				s.setScore(newScore);
+
+				if (getX() > b.getX() && getX() < b.getX() + b.getWidth()) {
+					dy = -dy;
+				}
+				// else if Ball y-coord is between the Brick's top and bottom edges
+				// reverse x-direction
+
+				else if (getY() > b.getY() && getY() < b.getY() + b.getHeight()) {
+					dx = -dx;
+				} else {
+					dy = -dy;
+					dx = -dx;
+				}
+
+			}
+
 		}
-	}
 
+	}
 
 }
--- src/breakout/BallWorld.java
+++ src/breakout/BallWorld.java
@@ -8,8 +8,10 @@ public class BallWorld extends World{
 	int width;
 	int height;
 	Score score;
-	int level;
+	int level=1;
 	Lives life;
+	boolean isPaused=true;
+	Paddle p;
 	public BallWorld(int w, int h){
 		width = w;
 		height = h;
@@ -20,10 +22,15 @@ public class BallWorld extends World{
 	//In your BallWorld's act() method, check if the number of Brick objects is zero.  If so, increment the level and load the bricks for that level.  If the level is 3 or more, take the user back to the main menu after they see the game over message and press a key.
 
 	public void act(long now) {
+		if (isKeyPressed(javafx.scene.input.KeyCode.SPACE) && isPaused) {
+			isPaused=false;
+		}
+		
+		
 		List<Brick> bricks = getObjects(Brick.class);
 		if(bricks.size()==0) {
-			if(level==3) {
-				
+			if(level>=3) {
+				 breakoutDriver.showIntroStage();
 			}
 			else {
 				level++;
@@ -48,7 +55,7 @@ public class BallWorld extends World{
 		b.setY(getHeight()/4);
 		add(b);
 		
-		Paddle p = new Paddle("paddle.png");
+		 p = new Paddle("paddle.png");
 		p.setX(getWidth()/2);
 		p.setY(getHeight()/2);
 		add(p);
@@ -71,7 +78,7 @@ public class BallWorld extends World{
 			}});
 
 		
-		score = new Score();
+		//score = new Score();
 		score.setX(20);
 		score.setY(40);
 		getChildren().add(score);
@@ -91,7 +98,17 @@ public class BallWorld extends World{
 		List<Brick> bricks = getObjects(Brick.class);
 		for(int i = 0;i<bricks.size();i++) {
 			bricks.remove(i);
+			remove(bricks.get(i));
 			i--;
 		}
+	}
+	public void setisPaused(boolean t) {
+		isPaused = t;
+	}
+	public boolean isPaused() {
+		return isPaused;
+	}
+	public Paddle getPaddle() {
+		return p;
 	}
 }
